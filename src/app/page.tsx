7f3fff695aec6e72437fcc275a5ad6939a582ae3
valdeir de melo <valdeirmelo@plenustech.com.br>
2024-12-17 09:17:01 -0400

    Commit New

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import { Button } from "@/src/components/ui/button"
+
+export default function Home() {
+  return (
+    <main>
+    <h1 className="text-2x1">Projeto Dashboard</h1>
+    <Button>
+    This is a Button
+    </Button>
+    </main>
+  );
+}
